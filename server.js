6c7505983a8f1457d12e0f46f26f436b45f33e94
amanda-spears <amanda_spears@hotmail.com>
2025-11-02 12:34:54 -0500

    Postman is working right

--- server.js
+++ server.js
@@ -12,12 +12,30 @@ app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use("/api/v1/getRandomImage", (request, response) => {
+async function getRandomImage() {
+    
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.client_id}`;
+        try {
+            const response = await fetch(endpoint);
+            const returnedData = await response.json()
+            const receivedPhotoUrl = returnedData.urls.regular;
+
+         //   const imgDiv = document.querySelector(".background-img");
+        //    imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+                return receivedPhotoUrl;
+    } catch (error) {
+            console.error(error)
+        }
+}
+
+
+app.use("/api/v1/getRandomImage",async (request, response) => {
     response.status(200).json({
         status: 200,
-        data: process.env.CLIENT_ID
+        data: await getRandomImage(),
     });
 });
+
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process.");
